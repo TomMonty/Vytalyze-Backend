17461db67fe8116f1c92e2402fd52dc8c2f65bf8
T_Monty <39499428+TomMonty@users.noreply.github.com>
2024-05-25 16:16:48 +0200

    Remove database password

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'Paris2024');
 define('DB_USER', 'root');
-define('DB_PASS', '#TomZoeLeo2209');
+define('DB_PASS', '');
 
 try {
     $pdo = new PDO(
